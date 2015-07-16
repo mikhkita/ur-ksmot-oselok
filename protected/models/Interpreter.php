90f760bf59dcdaeb2f763fad235c554b786fcf70
Михаил <beatbox787@gmail.com>
2015-07-16 19:03:44 +0600

    no message

--- protected/models/Interpreter.php
+++ protected/models/Interpreter.php
@@ -30,7 +30,7 @@ class Interpreter extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('name, template, good_type_id, rule_code', 'required'),
+			array('name, template, good_type_id', 'required'),
 			array('width, category_id', 'numerical', 'integerOnly'=>true),
 			array('name, rule_code', 'length', 'max'=>255),
 			array('template', 'length', 'max'=>20000),
@@ -166,81 +166,88 @@ class Interpreter extends CActiveRecord
 
     	preg_match_all("~\[\+([^\+\]]+)\+\]~", $template, $matches);
 
-		$rules = $matches[1];
+    	while( count($matches[1]) ){
 
-		foreach ($rules as $i => $rule) {
-			$tmp = explode(";", $rule);
-			$params = array();
-			foreach ($tmp as $param) {
-				$index = stripos($param, "=");
-				if( $index > 0 ){
-					$key = substr($param,0,$index);
-					$value = substr($param, $index+1);
-					$params[trim($key)] = $value;
-				}else{
-					throw new CHttpException(500,"Отсутствует знак \"=\" у параметра \"".$param."\" в интерпретаторе с идентификатором ".$interpreter_id);
-				}
-				
-			}
+			$rules = $matches[1];
 
-			if( isset($params["ALT"]) ){
-				$params["ALT"] = Interpreter::getArrayValue($params["ALT"],"ALT");
-			}
-
-			if( isset($params["REPLACE"]) ){
-				$params["REPLACE"] = Interpreter::getArrayValue($params["REPLACE"],"REPLACE");
-			}
-
-			if( isset($params["ATTR"]) ){
-				$val = "";
-
-				if( isset($attributes[intval($params["ATTR"])]) ){
-					$tmpArr = array();
-
-					if( is_array($attributes[intval($params["ATTR"])]) ){
-						foreach ($attributes[intval($params["ATTR"])] as $key => $v) {
-							$tmpArr[] = $v->value;
-						}
+			foreach ($rules as $i => $rule) {
+				$tmp = explode(";", $rule);
+				$params = array();
+				foreach ($tmp as $param) {
+					$index = stripos($param, "=");
+					if( $index > 0 ){
+						$key = substr($param,0,$index);
+						$value = substr($param, $index+1);
+						$params[trim($key)] = $value;
 					}else{
-						$tmpArr[] = $attributes[intval($params["ATTR"])]->value;
+						throw new CHttpException(500,"Отсутствует знак \"=\" у параметра \"".$param."\" в интерпретаторе с идентификатором ".$interpreter_id);
 					}
-
-					foreach ($tmpArr as $key => &$v) {
-						$v = ( isset($params["FLOAT"]) )?number_format((float)$v,intval($params["FLOAT"])):$v;
-
-						if( isset($params["REPLACE"]) ){
-							$v = str_replace($params["REPLACE"][0], $params["REPLACE"][1], $v);
-						}
-
-						$v = ( isset($params["ALT"]) && isset($params["ALT"][$v]) )?$params["ALT"][$v]:$v;
-					}
-					$val = implode( (isset($params["SEP"]))?$params["SEP"]:"/", $tmpArr);
+					
 				}
 
-				$matches[1][$i] = $val;
-			}else if( isset($params["INTER"]) ){
-				$matches[1][$i] = Interpreter::generate(intval($params["INTER"]),$model);
-			}else if( isset($params["LIST"]) ){
-				$matches[1][$i] = $this->getListValue(intval($params["LIST"]),$attributes);
-			}else if( isset($params["TABLE"]) ){
-				$matches[1][$i] = $this->getTableValue(intval($params["TABLE"]),$attributes);
-			}else if( isset($params["CUBE"]) ){
-				$matches[1][$i] = $this->getCubeValue(intval($params["CUBE"]),$attributes);
-			}else if( isset($params["VAR"]) ){
-				$matches[1][$i] = $this->getVarValue($params["VAR"]);
-			}else{
-				throw new CHttpException(500,"Отсутствует параметр \"ATTR\" у интерпретатора с идентификатором ".$interpreter_id);
+				if( isset($params["ALT"]) ){
+					$params["ALT"] = Interpreter::getArrayValue($params["ALT"],"ALT");
+				}
+
+				if( isset($params["REPLACE"]) ){
+					$params["REPLACE"] = Interpreter::getArrayValue($params["REPLACE"],"REPLACE");
+				}
+
+				if( isset($params["ATTR"]) ){
+					$val = "";
+
+					if( isset($attributes[intval($params["ATTR"])]) ){
+						$tmpArr = array();
+
+						if( is_array($attributes[intval($params["ATTR"])]) ){
+							foreach ($attributes[intval($params["ATTR"])] as $key => $v) {
+								$tmpArr[] = $v->value;
+							}
+						}else{
+							$tmpArr[] = $attributes[intval($params["ATTR"])]->value;
+						}
+
+						foreach ($tmpArr as $key => &$v) {
+							$v = ( isset($params["FLOAT"]) )?number_format((float)$v,intval($params["FLOAT"])):$v;
+
+							if( isset($params["REPLACE"]) ){
+								$v = str_replace($params["REPLACE"][0], $params["REPLACE"][1], $v);
+							}
+
+							$v = ( isset($params["ALT"]) && isset($params["ALT"][$v]) )?$params["ALT"][$v]:$v;
+						}
+						$val = implode( (isset($params["SEP"]))?$params["SEP"]:"/", $tmpArr);
+					}
+
+					$matches[1][$i] = $val;
+				}else if( isset($params["INTER"]) ){
+					$matches[1][$i] = Interpreter::generate(intval($params["INTER"]),$model);
+				}else if( isset($params["LIST"]) ){
+					$matches[1][$i] = $this->getListValue(intval($params["LIST"]),$attributes);
+				}else if( isset($params["TABLE"]) ){
+					$matches[1][$i] = $this->getTableValue(intval($params["TABLE"]),$attributes);
+				}else if( isset($params["CUBE"]) ){
+					$matches[1][$i] = $this->getCubeValue(intval($params["CUBE"]),$attributes);
+				}else if( isset($params["VAR"]) ){
+					$matches[1][$i] = $this->getVarValue($params["VAR"]);
+				}else{
+					throw new CHttpException(500,"Отсутствует параметр \"ATTR\" у интерпретатора с идентификатором ".$interpreter_id);
+				}
 			}
-		}
-		$out = str_replace($matches[0], $matches[1], $template);
+			$out = str_replace($matches[0], $matches[1], $template);
 
-		$calc = array(Interpreter::getArrayToCalculate($out),array());
-		foreach ($calc[0] as $key => $value) {
-			$calc[1][$key] = Interpreter::calculate($value);
-			$calc[0][$key] = '`'.$calc[0][$key]."`";
+			$calc = array(Interpreter::getArrayToCalculate($out),array());
+			foreach ($calc[0] as $key => $value) {
+				$calc[1][$key] = Interpreter::calculate($value);
+				$calc[0][$key] = '`'.$calc[0][$key]."`";
+			}
+
+			$template = str_replace($calc[0], $calc[1], $out);
+
+			preg_match_all("~\[\+([^\+\]]+)\+\]~", $template, $matches);
 		}
 
-		return str_replace($calc[0], $calc[1], $out);
+		return $template;
     }
 
     public function getArrayToCalculate($str){
