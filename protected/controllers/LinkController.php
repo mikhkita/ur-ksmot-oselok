6e809fd2686b153394644e7b8d3c688dd887db24
Vladislav Tsigel'nikov <tsigelnikovvlad@gmail.com>
2015-07-19 13:15:20 +0600

    no message

--- protected/controllers/LinkController.php
+++ protected/controllers/LinkController.php
@@ -40,19 +40,24 @@ class LinkController extends Controller
 			$links_arr = array();
 			//регулярнымвыражением парсим страницу, и находим все картники с расширением png и jpg
 			
-			$url = array_pop(explode("/",$url));
+			$url = array_pop(explode("/",$_POST['link']));
 			$url = str_replace(".html","",$url);
 			$dir = Yii::app()->params["imageFolder"]."/links/".$url;
-					if (!is_dir($dir)) mkdir($dir,0777, true);
-			foreach ($html->find('div[class=old_lot_images] img') as $i => $item) { 
-				copy( $item->src, $dir."/".$url."_".$i.".jpg");
-  			}
-  			$imgs = array_values(array_diff(scandir($dir), array('..', '.', 'Thumbs.db')));
-  			if(count($imgs)) {
-  				echo "1";
-  			}	else {
-  				echo "0";
-  			}
+			if (!is_dir($dir)) mkdir($dir,0777, true);
+			$imgs = array_values(array_diff(scandir($dir), array('..', '.', 'Thumbs.db')));
+			if(!count($imgs)) {
+				foreach ($html->find('div[class=old_lot_images] img') as $i => $item) { 
+					copy( $item->src, $dir."/".$url."_".$i.".jpg");
+	  			}
+	  			$imgs = array_values(array_diff(scandir($dir), array('..', '.', 'Thumbs.db')));
+	  			if(count($imgs)) {
+	  				echo "1";
+	  			}	else {
+	  				echo "0";
+	  			}
+	  		} else {
+	  			echo "2";
+	  		}
 		} else {
 			$this->render('adminIndex',array());
 		}
